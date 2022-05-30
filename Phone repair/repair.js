7d6eadd6eecbe4fcfd9edeacc39ee346ae0f2b8e
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-05-30 21:41:46 +0200

    main

--- Phone repair/repair.js
+++ Phone repair/repair.js
@@ -12,22 +12,22 @@ function myFunctionOpen() {
     document.getElementById('hero-opacity').style.filter = 'blur(5px)';
     document.getElementById('rel-opacity').style.filter = 'blur(5px)';
     document.getElementById('con-header').style.filter = 'blur(5px)';
-    document.getElementById('slider-opacity').style.filter = 'blur(5px)';
-    
-    
+    document.getElementById('slider-opacity').style.filter = 'blur(5px)';  
 }
 
 function myFunctionClose() {
     document.getElementById('call').style.display = 'none';
+    // opacity
     document.getElementById('rel-opacity').style.opacity = '1';
     document.getElementById('hero-opacity').style.opacity = '1';
     document.getElementById('con-header').style.opacity = '1';
     document.getElementById('slider-opacity').style.opacity = '1';
+    // overflow-y for body + background
     document.getElementById('over').style.overflowY = '';
     document.getElementById('over').style.background = '';
+    // blur for header + rel + con + hero
     document.getElementById('hero-opacity').style.filter = '';
     document.getElementById('rel-opacity').style.filter = '';
     document.getElementById('con-header').style.filter = '';
-    document.getElementById('slider-opacity').style.filter = '';
-    
+    document.getElementById('slider-opacity').style.filter = '';   
 }
