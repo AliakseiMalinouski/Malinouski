0d2178552db046a69b02a4c51bb0c3d35cb11e00
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-05-30 22:13:17 +0200

    main

--- Phone repair/repair.js
+++ Phone repair/repair.js
@@ -5,14 +5,16 @@ function myFunctionOpen() {
     document.getElementById('hero-opacity').style.opacity = '0.1';
     document.getElementById('con-header').style.opacity = '0.1';
     document.getElementById('slider-opacity').style.opacity = '0.1';
+    document.getElementById('repair').style.opacity = '0.1';
     // overflow-y for body + background
     document.getElementById('over').style.overflowY = 'hidden';
     document.getElementById('over').style.background = 'rgba(0,0,0,.7)';
-    // blur for header + rel + con + hero
+    // blur for header + rel + repair + hero
     document.getElementById('hero-opacity').style.filter = 'blur(5px)';
     document.getElementById('rel-opacity').style.filter = 'blur(5px)';
     document.getElementById('con-header').style.filter = 'blur(5px)';
-    document.getElementById('slider-opacity').style.filter = 'blur(5px)';  
+    document.getElementById('slider-opacity').style.filter = 'blur(5px)';
+    document.getElementById('repair').style.filter = 'blur(5px)';  
 }
 
 function myFunctionClose() {
@@ -22,12 +24,14 @@ function myFunctionClose() {
     document.getElementById('hero-opacity').style.opacity = '1';
     document.getElementById('con-header').style.opacity = '1';
     document.getElementById('slider-opacity').style.opacity = '1';
+     document.getElementById('repair').style.opacity = '1';
     // overflow-y for body + background
     document.getElementById('over').style.overflowY = '';
     document.getElementById('over').style.background = '';
-    // blur for header + rel + con + hero
+    // blur for header + rel + repair + hero
     document.getElementById('hero-opacity').style.filter = '';
     document.getElementById('rel-opacity').style.filter = '';
     document.getElementById('con-header').style.filter = '';
-    document.getElementById('slider-opacity').style.filter = '';   
+    document.getElementById('slider-opacity').style.filter = '';
+    document.getElementById('repair').style.filter = ''; 
 }
