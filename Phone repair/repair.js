497b3a6a4d4d38cdfc8daec12ee30687f07badf5
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-05-30 21:27:09 +0200

    main

--- Phone repair/repair.js
+++ Phone repair/repair.js
@@ -1,7 +1,18 @@
 function myFunctionOpen() {
-    document.getElementById('call').style.display = 'block'
+    document.getElementById('call').style.display = 'block';
+    document.getElementById('rel-opacity').style.opacity = '0.1';
+    document.getElementById('hero-opacity').style.opacity = '0.1';
+    document.getElementById('con-header').style.opacity = '0.1';
+    document.getElementById('slider-opacity').style.opacity = '0.1';
+    document.getElementById('over').style.overflowY = 'none';
+    
 }
 
 function myFunctionClose() {
-    document.getElementById('call').style.display = 'none'
+    document.getElementById('call').style.display = 'none';
+    document.getElementById('rel-opacity').style.opacity = '1';
+    document.getElementById('hero-opacity').style.opacity = '1';
+    document.getElementById('con-header').style.opacity = '1';
+    document.getElementById('slider-opacity').style.opacity = '1';
+    document.getElementById('over').style.overflowY = '1';
 }
