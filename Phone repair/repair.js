600fb1a7550d3d6a54151d8f21480a76679209c2
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-06-06 22:32:38 +0200

    main

--- Phone repair/repair.js
+++ Phone repair/repair.js
@@ -196,91 +196,23 @@ function CloseOtherProblemsMobile() {
 
 function OpenModalMobile() {
     document.getElementById('nav-header').style.display = 'block';
-    // opacity
-    document.getElementById('rel-opacity').style.opacity = '0.1';
-    document.getElementById('hero-opacity').style.opacity = '0.1';
-    // document.getElementById('con-header').style.opacity = '0.1';
-    document.getElementById('slider-opacity').style.opacity = '0.1';
-    document.getElementById('repair').style.opacity = '0.1';
-    document.getElementById('brands').style.opacity = '0.1';
-    document.getElementById('video').style.opacity = '0.1';
-    document.getElementById('opinion').style.opacity = '0.1';
-    document.getElementById('reviews').style.opacity = '0.1';
-    document.getElementById('sale').style.opacity = '0.1';
-    document.getElementById('master').style.opacity = '0.1';
-    document.getElementById('work').style.opacity = '0.1';
-    document.getElementById('our_service_center').style.opacity = '0.1';
-    document.getElementById('courier').style.opacity = '0.1';
-    document.getElementById('contacts').style.opacity = '0.1';
-    document.getElementById('footer').style.opacity = '0.1';
-    document.getElementById('footer-mobile').style.opacity = '0.1';
-    // overflow-y for body + background
+    document.getElementById('main').style.opacity = '0.1';
     document.getElementById('over').style.overflowY = 'hidden';
     document.getElementById('over').style.background = 'rgba(0,0,0,.7)';
     document.getElementById('none-fone').style.background = 'none';
-    // blur for header + rel + repair + hero
-    document.getElementById('hero-opacity').style.filter = 'blur(5px)';
-    document.getElementById('rel-opacity').style.filter = 'blur(5px)';
-    // document.getElementById('con-header').style.filter = 'blur(5px)';
-    document.getElementById('slider-opacity').style.filter = 'blur(5px)';
-    document.getElementById('repair').style.filter = 'blur(5px)';
-    document.getElementById('brands').style.filter = 'blur(5px)';
-    document.getElementById('video').style.filter = 'blur(5px)';
-    document.getElementById('opinion').style.filter = 'blur(5px)';
-    document.getElementById('reviews').style.filter = 'blur(5px)';
-    document.getElementById('sale').style.filter = 'blur(5px)';
-    document.getElementById('master').style.filter = 'blur(5px)';
-    document.getElementById('work').style.filter = 'blur(5px)';
-    document.getElementById('our_service_center').style.filter = 'blur(5px)';
-    document.getElementById('courier').style.filter = 'blur(5px)';
-    document.getElementById('contacts').style.filter = 'blur(5px)';
-    document.getElementById('footer').style.filter = 'blur(5px)';
-    document.getElementById('footer-mobile').style.filter = 'blur(5px)';
+    document.getElementById('main').style.filter = 'blur(5px)';
+    
     
 }
 
 
 function CloseModalMobile() {
     document.getElementById('nav-header').style.display = 'none';
-    document.getElementById('rel-opacity').style.opacity = '1';
-    document.getElementById('hero-opacity').style.opacity = '1';
-    document.getElementById('con-header').style.opacity = '1';
-    document.getElementById('slider-opacity').style.opacity = '1';
-    document.getElementById('repair').style.opacity = '1';
-    document.getElementById('brands').style.opacity = '1';
-    document.getElementById('video').style.opacity = '1';
-    document.getElementById('opinion').style.opacity = '1';
-    document.getElementById('reviews').style.opacity = '1';
-    document.getElementById('sale').style.opacity = '1';
-    document.getElementById('master').style.opacity = '1';
-    document.getElementById('work').style.opacity = '1';
-    document.getElementById('our_service_center').style.opacity = '1';
-    document.getElementById('courier').style.opacity = '1';
-    document.getElementById('contacts').style.opacity = '1';
-    document.getElementById('footer').style.opacity = '1';
-    document.getElementById('footer-mobile').style.opacity = '1';
-    
-    // overflow-y for body + background
+    document.getElementById('main').style.opacity = '1';
     document.getElementById('over').style.overflowY = '';
     document.getElementById('over').style.background = '';
     document.getElementById('none-fone').style.background = '';
-    // blur for header + rel + repair + hero
-    document.getElementById('hero-opacity').style.filter = '';
-    document.getElementById('rel-opacity').style.filter = '';
-    document.getElementById('con-header').style.filter = '';
-    document.getElementById('slider-opacity').style.filter = '';
-    document.getElementById('repair').style.filter = ''; 
-    document.getElementById('brands').style.filter = '';
-    document.getElementById('video').style.filter = ''; 
-    document.getElementById('opinion').style.filter = '';
-    document.getElementById('reviews').style.filter = '';
-    document.getElementById('sale').style.filter = '';
-    document.getElementById('master').style.filter = '';
-    document.getElementById('work').style.filter = '';
-    document.getElementById('our_service_center').style.filter = '';
-    document.getElementById('courier').style.filter = '';
-    document.getElementById('contacts').style.filter = '';
-    document.getElementById('footer').style.filter = '';
-    document.getElementById('footer-mobile').style.filter = '';
+    document.getElementById('main').style.filter = '';
+   
 }
 
