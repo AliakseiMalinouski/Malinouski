771e7693077e5d42afa48537e72f588b59017ee2
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-06-07 17:22:05 +0200

    main

--- Phone repair/repair.js
+++ Phone repair/repair.js
@@ -228,3 +228,176 @@ function myFunctionClose() {
     document.getElementById('footer-mobile').style.filter = '';
 }
 
+
+
+
+// loction1
+
+function OpenMapUnion1() {
+    document.getElementById('location_1').style.display = 'block';
+    document.getElementById('open-loc').style.display = 'none';
+
+
+ 
+}
+
+// location 2
+
+function OpenMapUnion2() {
+    document.getElementById('location_2').style.display = 'block';
+    document.getElementById('open-loc').style.display = 'none';
+}
+
+
+// location 3
+
+function OpenMapUnion3() {
+    document.getElementById('location_3').style.display = 'block';
+    document.getElementById('open-loc').style.display = 'none';
+    
+}
+
+// loction 4
+
+function OpenMapUnion4() {
+    document.getElementById('location_4').style.display = 'block';
+    document.getElementById('open-loc').style.display = 'none';
+    
+    
+}
+
+// location 5
+
+function OpenMapUnion5() {
+    document.getElementById('location_5').style.display = 'block';
+    document.getElementById('open-loc').style.display = 'none';
+
+}
+
+
+// location 6
+
+function OpenMapUnion6() {
+    document.getElementById('location_6').style.display = 'block';
+    document.getElementById('map').style.backgroundImage = 'none';
+    document.getElementById('union1').style.display = 'none';
+    document.getElementById('union2').style.display = 'none';
+    document.getElementById('union3').style.display = 'none';
+    document.getElementById('union4').style.display = 'none';
+    document.getElementById('union5').style.display = 'none';
+    document.getElementById('union6').style.display = 'none';
+    document.getElementById('union7').style.display = 'none';
+    document.getElementById('union8').style.display = 'none';
+    document.getElementById('union9').style.display = 'none';
+    document.getElementById('union10').style.display = 'none';
+    document.getElementById('union11').style.display = 'none';
+    document.getElementById('about-center').style.marginTop = '200px';
+}
+
+// location 7
+
+function OpenMapUnion7() {
+    document.getElementById('location_7').style.display = 'block';
+    document.getElementById('map').style.backgroundImage = 'none';
+    document.getElementById('union1').style.display = 'none';
+    document.getElementById('union2').style.display = 'none';
+    document.getElementById('union3').style.display = 'none';
+    document.getElementById('union4').style.display = 'none';
+    document.getElementById('union5').style.display = 'none';
+    document.getElementById('union6').style.display = 'none';
+    document.getElementById('union7').style.display = 'none';
+    document.getElementById('union8').style.display = 'none';
+    document.getElementById('union9').style.display = 'none';
+    document.getElementById('union10').style.display = 'none';
+    document.getElementById('union11').style.display = 'none';
+    document.getElementById('about-center').style.marginTop = '200px';
+}
+
+// location 8
+
+function OpenMapUnion8() {
+    document.getElementById('location_8').style.display = 'block';
+    document.getElementById('map').style.backgroundImage = 'none';
+    document.getElementById('union1').style.display = 'none';
+    document.getElementById('union2').style.display = 'none';
+    document.getElementById('union3').style.display = 'none';
+    document.getElementById('union4').style.display = 'none';
+    document.getElementById('union5').style.display = 'none';
+    document.getElementById('union6').style.display = 'none';
+    document.getElementById('union7').style.display = 'none';
+    document.getElementById('union8').style.display = 'none';
+    document.getElementById('union9').style.display = 'none';
+    document.getElementById('union10').style.display = 'none';
+    document.getElementById('union11').style.display = 'none';
+    document.getElementById('about-center').style.marginTop = '200px';
+}
+
+// location 9
+
+function OpenMapUnion9() {
+    document.getElementById('location_9').style.display = 'block';
+    document.getElementById('map').style.backgroundImage = 'none';
+    document.getElementById('union1').style.display = 'none';
+    document.getElementById('union2').style.display = 'none';
+    document.getElementById('union3').style.display = 'none';
+    document.getElementById('union4').style.display = 'none';
+    document.getElementById('union5').style.display = 'none';
+    document.getElementById('union6').style.display = 'none';
+    document.getElementById('union7').style.display = 'none';
+    document.getElementById('union8').style.display = 'none';
+    document.getElementById('union9').style.display = 'none';
+    document.getElementById('union10').style.display = 'none';
+    document.getElementById('union11').style.display = 'none';
+    document.getElementById('about-center').style.marginTop = '200px';
+}
+
+// location 10
+
+function OpenMapUnion10() {
+    document.getElementById('location_10').style.display = 'block';
+    document.getElementById('map').style.backgroundImage = 'none';
+    document.getElementById('union1').style.display = 'none';
+    document.getElementById('union2').style.display = 'none';
+    document.getElementById('union3').style.display = 'none';
+    document.getElementById('union4').style.display = 'none';
+    document.getElementById('union5').style.display = 'none';
+    document.getElementById('union6').style.display = 'none';
+    document.getElementById('union7').style.display = 'none';
+    document.getElementById('union8').style.display = 'none';
+    document.getElementById('union9').style.display = 'none';
+    document.getElementById('union10').style.display = 'none';
+    document.getElementById('union11').style.display = 'none';
+    document.getElementById('about-center').style.marginTop = '200px';
+}
+
+// location 11
+
+
+function OpenMapUnion11() {
+    document.getElementById('location_11').style.display = 'block';
+    document.getElementById('map').style.backgroundImage = 'none';
+    document.getElementById('union1').style.display = 'none';
+    document.getElementById('union2').style.display = 'none';
+    document.getElementById('union3').style.display = 'none';
+    document.getElementById('union4').style.display = 'none';
+    document.getElementById('union5').style.display = 'none';
+    document.getElementById('union6').style.display = 'none';
+    document.getElementById('union7').style.display = 'none';
+    document.getElementById('union8').style.display = 'none';
+    document.getElementById('union9').style.display = 'none';
+    document.getElementById('union10').style.display = 'none';
+    document.getElementById('union11').style.display = 'none';
+    document.getElementById('about-center').style.marginTop = '200px';
+}
+
+
+function CloseMapUnion() {
+    document.getElementById('location_1').style.display = 'none';
+    document.getElementById('open-loc').style.display = 'block';
+    document.getElementById('location_2').style.display = 'none';
+    document.getElementById('location_3').style.display = 'none';
+    document.getElementById('location_4').style.display = 'none';
+    document.getElementById('location_5').style.display = 'none';
+    
+}
+
