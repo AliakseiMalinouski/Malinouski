359072898f9bb445df897e73404e0203cb4383bc
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-06-08 18:23:39 +0200

    main

---
+++ Phone repair/sales.js
@@ -0,0 +1,8 @@
+function ThanskForFeedBackOpen() {
+    document.getElementById('thanks').style.display = 'block';
+    
+
+}
+function ThanskForFeedBackClose() {
+    document.getElementById('thanks').style.display = 'none';
+}
