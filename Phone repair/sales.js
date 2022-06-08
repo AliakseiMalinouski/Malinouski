40b6747f675714a8959b07f5edbb4579efe17dd2
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-06-08 20:23:10 +0200

    main

--- Phone repair/sales.js
+++ Phone repair/sales.js
@@ -8,23 +8,37 @@ function ThanskForFeedBackClose() {
 }
 function OpenModalMobile() {
     document.getElementById('nav-header').style.display = 'block';
-    document.getElementById('main').style.opacity = '0.1';
-    document.getElementById('over').style.overflowY = 'hidden';
-    document.getElementById('over').style.background = 'rgba(0,0,0,.7)';
-    document.getElementById('none-fone').style.background = 'none';
-    document.getElementById('main').style.filter = 'blur(5px)';
+    document.getElementById('change-display1').style.opacity = '0.1';
+    document.getElementById('change-display2').style.opacity = '0.1';
+    document.getElementById('change-display3').style.opacity = '0.1';
+    document.getElementById('repair-now').style.opacity = '0.1';
+    document.getElementById('footer-mobile').style.opacity = '0.1';
+    document.getElementById('change-display1').style.filter = 'blur(5px)';
+    document.getElementById('change-display2').style.filter = 'blur(5px)';
+    document.getElementById('change-display3').style.filter = 'blur(5px)';
+    document.getElementById('repair-now').style.filter = 'blur(5px)';
+    document.getElementById('footer-mobile').style.filter = 'blur(5px)';
     document.getElementById('logo').style.opacity = '0.1';
-    document.getElementById('logo').style.filter = 'blur(5px)';
+    
+    
+    
     
 }
 function CloseModalMobile() {
     document.getElementById('nav-header').style.display = 'none';
-    document.getElementById('main').style.opacity = '1';
-    document.getElementById('over').style.overflowY = '';
-    document.getElementById('over').style.background = '';
-    document.getElementById('none-fone').style.background = '';
-    document.getElementById('main').style.filter = '';
+    document.getElementById('change-display1').style.opacity = '1';
+    document.getElementById('change-display2').style.opacity = '1';
+    document.getElementById('change-display3').style.opacity = '1';
+    document.getElementById('repair-now').style.opacity = '1';
+    document.getElementById('footer-mobile').style.opacity = '1';
+    document.getElementById('change-display1').style.filter = '';
+    document.getElementById('change-display2').style.filter = '';
+    document.getElementById('change-display3').style.filter = '';
+    document.getElementById('repair-now').style.filter = '';
+    document.getElementById('footer-mobile').style.filter = '';
     document.getElementById('logo').style.opacity = '1';
-    document.getElementById('logo').style.filter = '';
+    
+    
+    
    
 }
