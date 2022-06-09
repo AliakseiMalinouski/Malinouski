c1081a0deb1e98f2565c443353a55691bb853b62
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-06-09 20:51:03 +0200

    main

---
+++ Phone repair/js/about-us.js
@@ -0,0 +1,18 @@
+let offset = 0;
+const SliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.next').addEventListener('click', function () {
+    offset = offset + 350;
+    if (offset > 700) {
+        offset = 0;
+    }
+    SliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    offset = offset - 350;
+    if (offset < 0 ) {
+        offset = 700;
+    }
+    SliderLine.style.left = -offset + 'px';
+});
