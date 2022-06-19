8d940cb5a793f01c8460837876e64e06e78ffa18
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-06-19 19:36:43 +0200

    main

--- computer-service/js/main.js
+++ computer-service/js/main.js
@@ -1,10 +1,11 @@
 let search = document.getElementById('search');
+let magnifier = document.getElementById('magnifier');
 document.getElementById('search').addEventListener('focus', function () {
     if (search.focus) {
-        document.getElementById('search').style.backgroundColor = 'lightblue';
+        document.getElementById('search').style.backgroundColor = '#FFFACD';
         document.getElementById('search').style.transform = 'scale(1.05)';
         document.getElementById('search').style.transition = '1s';
-        
+        document.getElementById('magnifier').style.left = '669px';
         
     }
 });
@@ -12,5 +13,6 @@ document.getElementById('search').addEventListener('blur', function () {
     if (search.blur) {
         document.getElementById('search').style.backgroundColor = 'white';
         document.getElementById('search').style.transform = '';
+        document.getElementById('magnifier').style.left = '640px';
     }
 });
