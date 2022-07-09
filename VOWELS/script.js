29333706dc350a736ec1c1cb6c0b95876dde42e4
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-07-09 19:47:35 +0200

    main

--- VOWELS/script.js
+++ VOWELS/script.js
@@ -1,19 +1,29 @@
 'use strict'
 
-function findLetter() {
+// function findLetter() {
+//     let count = 0;
+//     let arr = ['а', 'е', 'и', 'у', 'ы', 'ё', 'я', 'ю', 'о', 'A', 'И', 'Е', 'У', 'Ы', 'Ё', 'О', 'Ю', 'Я'];
+//     let message = prompt('Введите строку:');
+//     for (let i = 0; i < message.length; i++) {
+//         for (let n = 0; n < arr.length; n++) {
+//             if (message[i] === arr[n]) {
+//                 count++;
+//             }
+//         }
+//     }
+//     return count ? count : 'Не найдено';
+// }
+// console.log(findLetter());
+
+
+function findLetter(str) {
     let count = 0;
     let arr = ['а', 'е', 'и', 'у', 'ы', 'ё', 'я', 'ю', 'о', 'A', 'И', 'Е', 'У', 'Ы', 'Ё', 'О', 'Ю', 'Я'];
-    let message = prompt('Введите строку:');
-    for (let i = 0; i < message.length; i++) {
-        for (let n = 0; n < arr.length; n++) {
-            if (message[i] === arr[n]) {
-                count++;
-            }
+    for (let letter of str.toLowerCase()) {
+        if (arr.includes(letter)) {
+            count++;
         }
     }
-    return count ? count : 'Не найдено';
+    return count;
 }
-console.log(findLetter());
-
-
-
+console.log(findLetter('КоличЕствО гласнЫх в этой строкЕ'));
