865cecf9e348af646b7565c1183ce174d40dd7e1
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-07-09 16:04:25 +0200

    main

--- VOWELS/script.js
+++ VOWELS/script.js
@@ -2,7 +2,7 @@
 
 function findLetter() {
     let count = 0;
-    let arr = ['a', 'е', 'и', 'у', 'ы', 'ё', 'я', 'ю', 'о', 'A', 'И', 'Е', 'У', 'Ы', 'Ё'];
+    let arr = ['а', 'е', 'и', 'у', 'ы', 'ё', 'я', 'ю', 'о', 'A', 'И', 'Е', 'У', 'Ы', 'Ё'];
     let message = prompt('Введите строку:');
     for (let i = 0; i < message.length; i++) {
         for (let n = 0; n < arr.length; n++) {
