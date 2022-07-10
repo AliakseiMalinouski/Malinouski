e358e5a5366c623420a019b75d913fdee9276b25
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-07-10 09:57:40 +0200

    main

--- treeSum/script.js
+++ treeSum/script.js
@@ -1,20 +1,38 @@
 "use strict"
 
-let arr =  [ 5, 7, 
-        [ 4, [2], 8, [1,3], 2 ], 
-        [ 9, [] ], 
-        1, 8
-      ]
-let sum = 0;
-arr = arr.flat(Infinity);
-function treeSum() {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr.length == 0) {
-            return 0;
+
+// function treeSum() {
+//     let arr =  [ 5, 7,
+//         [ 4, [2], 8, [1,3], 2 ],
+//         [ 9, [] ],
+//         1, 8
+//       ]
+//     let sum = 0;
+//     arr = arr.flat(Infinity);
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr.length == 0) {
+//             return 0;
+//         }
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+// console.log(treeSum());
+
+function treeSum(arr) {
+    let sum = 0;
+    for (let element of arr) {
+        if (typeof element == 'object') {
+            sum += treeSum(element);
         }
-        sum += arr[i];
+        else {
+            sum += element;
+        }
     }
     return sum;
 }
-console.log(treeSum())
-
+console.log(treeSum([ 5, 7,
+        [ 4, [2], 8, [1,3], 2 ],
+        [ 9, [] ],
+        1, 8
+    ]));
