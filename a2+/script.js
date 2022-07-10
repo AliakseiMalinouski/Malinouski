f877838fe3415d99b31f56aadd552660c6ad02a6
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-07-10 16:16:24 +0200

    main

---
+++ a2+/script.js
@@ -0,0 +1,9 @@
+'use strict'
+let str = prompt('Введите строку:');
+
+function deleteWhiteSpace(str){
+    let ser = str.split(' ');
+    return ser.filter(i => i).join(' ');
+    console.log(ser);
+}
+alert(`Строка без пробелов: ${deleteWhiteSpace(str)}`);
