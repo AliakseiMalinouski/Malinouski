612c1187dafc3ba6af9e5631a7264bcfc8cd89c1
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-07-17 10:04:43 +0200

    main

--- VOWELS_ARR/script.js
+++ VOWELS_ARR/script.js
@@ -1,5 +1,5 @@
 'use strict'
-let message = prompt('Введите строку:')
+let message = prompt('Введите первую строку:')
 function vowelsForEach() {
     let count = 0;
     let arr = ['а', 'е', 'и', 'у', 'ы', 'ё', 'я', 'ю', 'о', 'A', 'И', 'Е', 'У', 'Ы', 'Ё', 'О', 'Ю', 'Я'];
@@ -11,5 +11,38 @@ function vowelsForEach() {
     });
     return count;
 }
-console.log('Количество гласных в строке:');
+console.log('Количество гласных в первой строке:');
 console.log(vowelsForEach());
+
+console.log('----------------------------------');
+
+let message2 = prompt('Введите вторую строку:');
+function vowelsFilter() {
+    let cnt = 0;
+    let array = ['а', 'е', 'и', 'у', 'ы', 'ё', 'я', 'ю', 'о', 'A', 'И', 'Е', 'У', 'Ы', 'Ё', 'О', 'Ю', 'Я'];
+    array.filter(function (element) {
+        let message3 = message2.toLocaleLowerCase();
+        if (message3.includes(element)) {
+            cnt++;
+        }
+    });
+    return cnt;
+}
+console.log('Количество гласных во второй строке:');
+console.log(vowelsFilter());
+
+console.log('----------------------------------');
+
+let message4 = prompt('Введите третью строку:');
+let message4L = message4.toLowerCase();
+let ar = ['а', 'е', 'и', 'у', 'ы', 'ё', 'я', 'ю', 'о', 'A', 'И', 'Е', 'У', 'Ы', 'Ё', 'О', 'Ю', 'Я'];
+let sumVowels = ar.reduce(function (prev, item, index, array) {
+    let cn = 0;
+    for (let i = 0; i < message4L.length; i++) {
+        if (ar[item] === message4L[i]) {
+            cn++;
+        }
+    }
+    return cn;
+}, 0);
+console.log(sumVowels);
