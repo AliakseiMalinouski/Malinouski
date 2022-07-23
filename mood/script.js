c3dbc7cd9775aa7e2b40b40ab889bcb4e6d026a9
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-07-23 13:20:16 +0200

    main

--- mood/script.js
+++ mood/script.js
@@ -1,25 +1,21 @@
 "use strict";
 
-function randomDiap(n,m) {
-    return Math.floor(Math.random()*(m-n+1))+n;
-}
-
-function mood(colorsCount) {
-    var colors=[ '', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый' ];
-    console.log( 'цветов: ' + colorsCount );
-    for (var i = 1; i <= colorsCount; i++) {
-        var colorsObject = {
-            red: 'красный',
-            orange: 'оранжевый',
-            yellow: 'желтый',
-            green: 'зеленый',
-            blue: 'голубой',
-            sky: 'синий',
-            purple: 'фиолетовый'};
-            var n=randomDiap(1, colors.length - 1);
-            var colorName = colors[n];
-            var colorName = colors.splice(n, 1)[0];
-            console.log( colorName );
+    function randomDiap(n,m) {
+            return Math.floor(Math.random()*(m-n+1))+n;
     }
+
+    function mood(colorsCount) {
+        var colors=[ '', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый' ];
+
+        console.log( 'цветов: ' + colorsCount );
+        for (var i = 1; i <= colorsCount; i++) {
+            var colorsObj = {};
+            colorsObj[n] = true;
+            do {
+                var n=randomDiap(1,7);
+            } while (n in colorsObj);
+            var colorName=colors[n];
+            console.log( colorName );
+        }
 }
 mood(3);
