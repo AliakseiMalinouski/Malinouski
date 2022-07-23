835dae770a250bef130d2fb67f4d9ce6516d544d
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-07-23 16:59:07 +0200

    main

---
+++ DRINKS_HASH_FUNC/js/script.js
@@ -0,0 +1,7 @@
+"use strict"
+function HashStorageFunc(nameDrink, typeDrink, wayDrink) {
+    let self = this;
+}
+let objDrink = new HashStorageFunc(); // здесь информация полученная от пользователя
+HashStorageFunc()
+
