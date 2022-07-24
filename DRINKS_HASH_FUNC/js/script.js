9fd71efab0853f9e6d60839c77baa8ff45a0f784
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-07-24 19:59:54 +0200

    main

--- DRINKS_HASH_FUNC/js/script.js
+++ DRINKS_HASH_FUNC/js/script.js
@@ -1,7 +1,20 @@
 "use strict"
-function HashStorageFunc(nameDrink, typeDrink, wayDrink) {
-    let self = this;
-}
-let objDrink = new HashStorageFunc(); // здесь информация полученная от пользователя
-HashStorageFunc()
+
+document.querySelector(".info_item").addEventListener('click', function () {
+    let nameDrinkS = prompt('Введите название напитка:');
+    let nameDrink = nameDrinkS.trim().toLocaleLowerCase();
+    let objTypeAndWay = {};
+    objTypeAndWay.name = nameDrink;
+    objTypeAndWay.type = confirm(nameDrink + ' - это алкольный напиток?');
+    if (objTypeAndWay[1] === false) {
+        objTypeAndWay.type = 'Неалкогольный';
+    }
+    else {
+        objTypeAndWay.type = 'Алкогольный';
+    }
+    let wayS = prompt('Введите, пожалуйста, рецепт вашего напитка:');
+    let way = wayS.trim().toLocaleLowerCase();
+    objTypeAndWay.way = way;
+    console.log(objTypeAndWay);
+});
 
