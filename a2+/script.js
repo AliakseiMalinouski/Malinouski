09347aaf720bbb45be309e7c673c82e3658fed4f
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-08-08 09:59:58 +0200

    main

--- a2+/script.js
+++ a2+/script.js
@@ -8,12 +8,37 @@ let str = prompt('Введите строку:');
 // alert(`Строка без пробелов: ${deleteWhiteSpace(str)}`);
 
 
-function deleteWhiteSpace(str) {
-    if (str.charAt(0) == ' ' || str.charAt(str.lenght - 1) == ' ') {
-        str = str.slice(1);
-        str = deleteWhiteSpace(str);
+// function deleteWhiteSpace(str) {
+//     if (str.charAt(0) == ' ' || str.charAt(str.lenght - 1) == ' ') {
+//         str = str.slice(1);
+//         str = deleteWhiteSpace(str);
+//     }
+//     return str;
+// }
+
+// alert(`Строка без пробелов: ${deleteWhiteSpace(str)}`);
+
+let deleteWhiteSpace = function (str) {
+    let firstChar = undefined;
+    let lastChar;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== ' ') {
+            firstChar = i;
+            // console.log(str[i]); проверка
+            break;
+        }
     }
-    return str;
-} 
+    if (firstChar === undefined) {
+        return "";
+    }
+    for (let i = str.length - 1; ; --i) {
+        if (str[i] !== ' ') {
+            lastChar = i;
+            // console.log(str[i]); проверка
+            break;
+        }
+    }
+    return str.substring(firstChar, lastChar + 1);
+}
 
 alert(`Строка без пробелов: ${deleteWhiteSpace(str)}`);
