0489adef7be03be69078ced62fc681a44a629df4
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-08-10 16:12:17 +0200

    main

--- a3+/script.js
+++ a3+/script.js
@@ -3,7 +3,8 @@ let str = prompt('Введите слово:');
 function findPol(str) {
     str = str.toLowerCase().replace(/ё/g, "е").replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()ъь]/g, "").replace(/\s/g, "");
     let lastChar = str.length - 1;
-    for (let i = 0; i < str.length / 2; i++) {
+    for (let i = 0; i < Math.floor(str.length / 2); i++) {
+        console.log(str[i])
         if (str[i] !== str[lastChar - i]) {
             return false;
         }
