f2e979c43a3039e8f922634978def8c1eac9781c
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-08-10 10:12:35 +0200

    main

--- a3+/script.js
+++ a3+/script.js
@@ -1,17 +1,14 @@
 "use strict"
-let str = prompt('Введите слово:').trim().toLowerCase().replace(/ё/g, "е").replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()ъь]/g, "");
+let str = prompt('Введите слово:');
 function findPol(str) {
-    let ser = '';
-    for (let i = str.length - 1; i >= 0; --i) {
-        ser += str[i];
+    str = str.toLowerCase().replace(/ё/g, "е").replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()ъь]/g, "").replace(/\s/g, "");
+    let lastChar = str.length - 1;
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[lastChar - i]) {
+            return `Слово ${str} не палиндром`;
+        }
     }
-    if (str == ser) {
-        alert(`Слово: ${str} - палиндром`);
-    }
-    if(str !== ser) {
-        alert(`Слово: ${str} не является палиндромом`);
-    }
-    return str == ser;
+    return `Слово ${str} - это палиндром`;
 }
-// проверка
-console.log(findPol(str)); 
+alert(findPol(str));
+console.log(findPol(str));
