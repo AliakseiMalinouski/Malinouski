98db9d8759381851d8ba461a22f6d641283c1a15
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-08-22 14:25:29 +0200

    main

--- DRAGNDROP/script.js
+++ DRAGNDROP/script.js
@@ -2,25 +2,28 @@
 window.addEventListener('load', changePos);
 function changePos(EO) {
     EO = EO || window.event;
-    let dragElement = document.getElementById('t');
+    let imgs = document.querySelectorAll('img');
     let offsetX;
     let offsetY;
-    dragElement.onmousedown = function (EO) {
-        EO = EO || window.event;
-        offsetX = EO.offsetX;
-        offsetY = EO.offsetY;
-        dragElement.style.position = 'absolute';
-        dragElement.style.zIndex = '1';
-    }
-    dragElement.onmousemove = function (EO) {
-        EO = EO || window.event;
-        let x = EO.pageX;
-        let y = EO.pageY;
-        dragElement.style.left = (x - offsetX) + 'px';
-        dragElement.style.top = (y - offsetY) + 'px';
-    }
-    dragElement.onmouseup = function (EO) {
-        EO = EO || window.event;
-        dragElement = null;
+    for (let i = 0; i < imgs.length; i++) {
+        let dragElement = imgs[i];
+        dragElement.onmousedown = function (EO) {
+            EO = EO || window.event;
+            offsetX = EO.offsetX;
+            offsetY = EO.offsetY;
+            dragElement.style.position = 'absolute';
+            dragElement.style.zIndex = '1';
+        }
+        dragElement.onmousemove = function (EO) {
+            EO = EO || window.event;
+            let x = EO.pageX;
+            let y = EO.pageY;
+            dragElement.style.left = (x - offsetX) + 'px';
+            dragElement.style.top = (y - offsetY) + 'px';
+        }
+        dragElement.onmouseout = function (EO) {
+            EO = EO || window.event;
+            dragElement = null;
+        }
     }
 }
