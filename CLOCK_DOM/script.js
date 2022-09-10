0025b9c73b31af025f901fe4f6889f7d640be0b4
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-09-10 18:08:22 +0200

    main

--- CLOCK_DOM/script.js
+++ CLOCK_DOM/script.js
@@ -7,15 +7,15 @@ timer.setAttribute('class', 'timer');
 clock.appendChild(timer);
 buildClock.addEventListener('click', function (EO) {
     EO = EO || window.event;
-    clock.style.display = 'block';
     let sizeValue = size.value;
+    let cx = sizeValue/2;
+    let r = 160;
+    let dr = 30;
+    EO = EO || window.event;
+    clock.style.display = 'block';
     clock.style.width = sizeValue + 'px';
     clock.style.height = sizeValue + 'px';
-});
-let cx = 210;
-let r = 160;
-let dr = 30;
-for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 12; i++) {
     let arrayHours = [6, 5, 4, 3, 2, 1, 12, 11, 10, 9, 8, 7];
     let angle = i * 360 / 12 / 180 * Math.PI;
     let x = cx + r * Math.sin(angle);
@@ -25,13 +25,12 @@ for (let i = 0; i < 12; i++) {
     nextHourDiv.style.left = (x - dr) + 'px';
     nextHourDiv.style.top = (y - dr) + 'px';
     clock.appendChild(nextHourDiv);
-    // nextHourDiv.style.transform = `rotate(${i * 30}deg)`;
     let number = document.createElement('span');
     number.setAttribute('class', 'number');
     number.textContent = arrayHours[i];
-    // number.style.transform = `rotate(-${i * 50}deg)`;
     nextHourDiv.appendChild(number);
 }
+});
 let divHands = document.createElement('div');
 let hoursHand = document.createElement('div');
 hoursHand.setAttribute('class', 'hours');
