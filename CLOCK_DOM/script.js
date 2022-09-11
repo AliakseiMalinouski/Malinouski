401db2c058102f5fe6e4e7196aa0e66e84b7f38d
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-09-11 14:04:13 +0200

    main

--- CLOCK_DOM/script.js
+++ CLOCK_DOM/script.js
@@ -9,25 +9,25 @@ buildClock.addEventListener('click', function (EO) {
     EO = EO || window.event;
     let sizeValue = size.value;
     let cx = sizeValue/2;
-    let r = sizeValue/2 - 25;
+    let r = sizeValue/2 - 20;
     EO = EO || window.event;
     clock.style.display = 'block';
     clock.style.width = sizeValue + 'px';
     clock.style.height = sizeValue + 'px';
     for (let i = 0; i < 12; i++) {
-    let arrayHours = [6, 5, 4, 3, 2, 1, 12, 11, 10, 9, 8, 7];
     let angle = i * 360 / 12 / 180 * Math.PI;
     let x = cx + r * Math.sin(angle);
-    let y = cx + r * Math.cos(angle);
+    let y = cx - r * Math.cos(angle);
     let nextHourDiv = document.createElement('div');
     nextHourDiv.setAttribute('class', 'next-our-div');
-    let dr = 27;
+    let dr = 20;
     nextHourDiv.style.left = (x - dr) + 'px';
     nextHourDiv.style.top = (y - dr) + 'px';
     clock.appendChild(nextHourDiv);
     let number = document.createElement('span');
     number.setAttribute('class', 'number');
-    number.textContent = arrayHours[i];
+    // let numbers = [12,1,2,3,4,5,6,7,8,9,10,11];
+    // number.textContent = numbers[i];
     nextHourDiv.appendChild(number);
 }
 });
