0a6c09289ac9d6fed0b11ba3c955c17df339efa8
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-09-14 18:08:07 +0200

    main

---
+++ TENNIS_DOM/script.js
@@ -0,0 +1,28 @@
+"use strict"
+"use strict"
+let gameZone = document.getElementById('wrapper-game');
+let start = document.getElementById('start');
+let rocketRight = document.getElementById('rocket-right');
+let rocketLeft = document.getElementById('rocket-left');
+let ball = document.getElementById('ball');
+document.addEventListener('keydown', moveRocketLeft);
+let counterRocketLeftTop = 25;
+let counterRocketLeftMinusTop = 0;
+function moveRocketLeft(EO) {
+    EO = EO || window.event;
+    let rocketLeftMaxTop = parseInt(getComputedStyle(rocketLeft).getPropertyValue('top'));
+    if (EO.code == 'ControlLeft') {
+        counterRocketLeftTop = rocketLeftMaxTop;
+        counterRocketLeftTop += 5;
+        rocketLeft.style.top = counterRocketLeftTop + 'px';
+    }
+    if (rocketLeftMaxTop > 170) {
+        rocketLeft.style.top = '25px';
+        counterRocketLeftTop = 25;
+    }
+    if (EO.code == 'ShiftLeft') {
+        counterRocketLeftMinusTop = rocketLeftMaxTop;
+        counterRocketLeftMinusTop -= 5;
+        rocketLeft.style.top = counterRocketLeftMinusTop + 'px';
+    }
+}
