d98013678bb71a2134fdbea8d52421a55699c420
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-09-15 21:17:15 +0200

    main

--- GameLibrary/js/tictactoe.js
+++ GameLibrary/js/tictactoe.js
@@ -195,4 +195,10 @@ function changePositionTableWinner(EO) {
     } 
     setTimeout(changePositionTableWinner, 80);
 }
-
+// получаем ширину окна 
+let widthWindow = window.innerWidth;
+let heigthWindow = window.innerHeight;
+console.log(widthWindow, heigthWindow)
+if (widthWindow < 1202) {
+    changePositionTableWinner = null;
+}
