6018c3821b47cb30d4b8e6fbaa7d85164a876589
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-09-18 11:40:59 +0200

    main

--- b6+/script.js
+++ b6+/script.js
@@ -1,30 +1,12 @@
 "use strict";
-function buildWrapper(tag) {
-  function wrapFunc(text, getObj) {
-    let kov = '';
-    let objMnemonic = {
-      '"': '&quot;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '&': '&amp;',
-      "'": '&apos;',
-    };
-    for (let k in getObj) {
-      let attribute = ` ${k}="${getObj[k].replace(/&/g, '&amp;').replace(/'/g, '&apos;').replace(/"/g, '&&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;')}"`;
-      kov += attribute;
-    }
-    for (let i = 0; i < text.length; i++) {
-      if (text[i] in objMnemonic) {
-        text = text.split(text[i]).join(objMnemonic[text[i]]);
-      }
-    }
-    let start = kov ? `<${tag}${kov}>` : `<${tag}>`;
-    let end = `</${tag}>`;
-    return `${start}${text}${end}`;
+let buildWrapper = function (tag) {
+  return (text, getObj) => {
+    let attribute = '';
+    if (typeof getObj === 'object') for (let i in getObj) attribute += ` ${i}="${getObj[i].replace(/&/g, '&amp;').replace(/'/g, "&apos;")}"`;
+    return('<'+ tag + attribute +'>' + text.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;") + '</'+ tag +'>');
   }
-  return wrapFunc;
 }
-let wrapP = buildWrapper('P');
+let wrapH1=buildWrapper("H1");
+let wrapP=buildWrapper("P");
 console.log(wrapP("Однажды в <студёную> зимнюю пору", { lang: "ru" }));
-let wrapH1 = buildWrapper('H1');
 console.log(wrapH1('СТИХИ', {align:"center",title:"M&M's"}));
