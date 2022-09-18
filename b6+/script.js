ba4f5aaeffdbe88a69da27ee1dfecdf7d1c36cf2
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-09-18 11:43:34 +0200

    main

--- b6+/script.js
+++ b6+/script.js
@@ -3,10 +3,10 @@ let buildWrapper = function (tag) {
   return (text, getObj) => {
     let attribute = '';
     if (typeof getObj === 'object') for (let i in getObj) attribute += ` ${i}="${getObj[i].replace(/&/g, '&amp;').replace(/'/g, "&apos;")}"`;
-    return('<'+ tag + attribute +'>' + text.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;") + '</'+ tag +'>');
+    return `<${tag}${attribute}>${text.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;")}</${tag}>`;
   }
 }
-let wrapH1=buildWrapper("H1");
-let wrapP=buildWrapper("P");
+let wrapP = buildWrapper("P");
+let wrapH1 = buildWrapper("H1");
 console.log(wrapP("Однажды в <студёную> зимнюю пору", { lang: "ru" }));
 console.log(wrapH1('СТИХИ', {align:"center",title:"M&M's"}));
