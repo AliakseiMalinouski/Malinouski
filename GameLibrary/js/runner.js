9dff566a0f24ab3ff9ed6ff5424e47a7045d188a
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-09-29 17:33:41 +0200

    main

--- GameLibrary/js/runner.js
+++ GameLibrary/js/runner.js
@@ -308,7 +308,6 @@ setInterval(function () {
     jo1.classList.add('animation__for__jo1');
     jo2.classList.add('animation__for__jo2')
     jo3.classList.add('animation__for__jo3');
-    checkClassName();
 }, 1000);
 setInterval(function checkClassName() {
     if (jo1.classList == 'animation__for__jo1') {
