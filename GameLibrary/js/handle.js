fe49b7b4c3b0ef356d14d67d1d3824c51cfbe2b3
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-09-30 14:54:21 +0200

    main

--- GameLibrary/js/handle.js
+++ GameLibrary/js/handle.js
@@ -9,6 +9,14 @@ let firstEnemyAnimal = document.createElement('img');
 firstEnemyAnimal.setAttribute('src', './imgs/animal1.png');
 firstEnemyAnimal.classList.add('first__enemy__animal');
 areaGame.appendChild(firstEnemyAnimal);
+let secondEnemyAnimal = document.createElement('img');
+secondEnemyAnimal.setAttribute('src', './imgs/animal2.png');
+secondEnemyAnimal.classList.add('second__enemy__animal');
+areaGame.appendChild(secondEnemyAnimal);
+let thirdEnemyAnimal = document.createElement('img');
+thirdEnemyAnimal.setAttribute('src', './imgs/animal3.png');
+thirdEnemyAnimal.classList.add('third__enemy__animal');
+areaGame.appendChild(thirdEnemyAnimal);
 // добавляем score
 let flag = false;
 let scoreSpan = document.createElement('div');
@@ -17,30 +25,65 @@ scoreSpan.innerHTML = score;
 areaGame.appendChild(scoreSpan);
 // добавляем звуки
 let mouseDownOnAnimal = new Audio('./audio/handleClick.mp3');
-// анимируем бобров
-let startTopPositionFirstEnemyAnimal = 380;
+// анимируем всех вражеских животных
+// начальные позиции по оси y
+let startTopPositionFirstEnemyAnimal1 = 380;
+let startTopPositionFirstEnemyAnimal2 = 370;
+let startTopPositionFirstEnemyAnimal3 = 440;
 function animateFirstEnemyAnimal() {
-    startTopPositionFirstEnemyAnimal = startTopPositionFirstEnemyAnimal - 0.2;
-    firstEnemyAnimal.style.top = startTopPositionFirstEnemyAnimal + 'px';
-    if (startTopPositionFirstEnemyAnimal < 295) {
+    startTopPositionFirstEnemyAnimal1 = startTopPositionFirstEnemyAnimal1 - 0.2;
+    firstEnemyAnimal.style.top = startTopPositionFirstEnemyAnimal1 + 'px';
+    if (startTopPositionFirstEnemyAnimal1 < 285) {
         firstEnemyAnimal.style.display = 'block';
     }
-    if (startTopPositionFirstEnemyAnimal <= 280) {
+    if (startTopPositionFirstEnemyAnimal1 <= 280) {
         firstEnemyAnimal.style.top = '280px';
         setTimeout(function () {
-        startTopPositionFirstEnemyAnimal = 380;
+        startTopPositionFirstEnemyAnimal1 = 380;
         firstEnemyAnimal.style.display = 'none';
         }, 1000);
     }
     setTimeout(animateFirstEnemyAnimal, 0);
 }
 animateFirstEnemyAnimal();
-firstEnemyAnimal.addEventListener('click', function t (EO) {
-    EO = EO || window.event;
-    mouseDownOnAnimal.play();
-    score++;
-    scoreSpan.innerHTML = score;
-    flag = true;
-    
-});
+// анимируем медведя
+function animateSecondEnemyAnimal() {
+    startTopPositionFirstEnemyAnimal2 = startTopPositionFirstEnemyAnimal2 - 0.5;
+    secondEnemyAnimal.style.top = startTopPositionFirstEnemyAnimal2 + 'px';
+    if (startTopPositionFirstEnemyAnimal2 < 265) {
+        secondEnemyAnimal.style.display = 'block';
+    }
+    if (startTopPositionFirstEnemyAnimal2 <= 250) {
+        secondEnemyAnimal.style.top = '250px';
+        setTimeout(function () {
+            startTopPositionFirstEnemyAnimal2 = 370;
+            secondEnemyAnimal.style.display = 'none';
+        }, 1000);
+    }
+    setTimeout(animateSecondEnemyAnimal, 0);
+}
+animateSecondEnemyAnimal();
+function animateThirdEnemyAnimal() {
+    startTopPositionFirstEnemyAnimal3 = startTopPositionFirstEnemyAnimal3 - 0.3;
+    thirdEnemyAnimal.style.top = startTopPositionFirstEnemyAnimal3 + 'px';
+    if (startTopPositionFirstEnemyAnimal3 < 320) {
+        thirdEnemyAnimal.style.display = 'block';
+    }
+    if (startTopPositionFirstEnemyAnimal3 <= 300) {
+        thirdEnemyAnimal.style.top = '300px';
+        setTimeout(function () {
+            startTopPositionFirstEnemyAnimal3 = 440;
+            thirdEnemyAnimal.style.display = 'none'; 
+        }, 1000);
+    }
+    setTimeout(animateThirdEnemyAnimal, 0);
+}
+animateThirdEnemyAnimal();
+// firstEnemyAnimal.addEventListener('click', function t (EO) {
+//     EO = EO || window.event;
+//     mouseDownOnAnimal.play();
+//     score++;
+//     scoreSpan.innerHTML = score;
+//     flag = true;
+// });
 
