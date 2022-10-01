92805781f42badabeae6d9364b93173d9dc9b9fc
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-10-02 00:59:44 +0200

    main

--- GameLibrary/js/testing.js
+++ GameLibrary/js/testing.js
@@ -1,6 +1,10 @@
 "use strict";
+let btn = document.getElementById('btn');
 let canvas = document.getElementById('canvas');
 let contextOfCanvas = canvas.getContext('2d');
+btn.onclick = function () {
+    canvas.style.display = 'block';
+}
 // создаём хэш с данными о canvas
 let hCanvas = {
     width: canvas.width,
@@ -21,12 +25,14 @@ let hHuman = {
     lastname: 'Run',
     profession: 'archaeologist',
     transform: 90,
+    universe: 'JoeRun Game',
 };
 contextOfCanvas.fillStyle = 'white';
 contextOfCanvas.font = '24px bold Sans-serif';
 let textNameJoe = contextOfCanvas.fillText('Name: ' + hHuman.name, 10, 30);
 let textLastNameJoe = contextOfCanvas.fillText('Lastname: ' + hHuman.lastname, 10, 60);
 let textProfessionJoe = contextOfCanvas.fillText('Profession: ' + hHuman.profession, 10, 90);
+let textUniverseJoe = contextOfCanvas.fillText('Universe: ' + hHuman.universe, 10, 120);
 function drawCanvasElements() {
     contextOfCanvas.drawImage(human, hHuman.x, hHuman.y, hHuman.width, hHuman.height,);
     hHuman.x = hHuman.x + 1;
