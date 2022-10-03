92a50249a4c443578cf6773532c1461785ecbf3d
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-10-03 21:00:05 +0200

    main

--- GameLibrary/js/main.js
+++ GameLibrary/js/main.js
@@ -1,12 +1,13 @@
 "use strict";
 // делаем поиск игр
 let search = document.getElementById('search');
+let containerSearchBlock = document.querySelector('container__search__block');
+let searchVariants = document.querySelectorAll('.search__variants li');
 search.oninput = function (EO) {
     EO = EO || window.event;
     search.value = search.value.toLowerCase(); // обрезаем пробелы и сводим всё к нижнему регистру, меняем спец.символы на html мнемоники
     search.value = search.value.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
     let value = this.value.trim();
-    let searchVariants = document.querySelectorAll('.search__variants li');
     if (value != '') {
         searchVariants.forEach(function (element) {
             if (element.textContent.search(value) == -1) {
@@ -28,9 +29,10 @@ search.oninput = function (EO) {
         });
     }
     if (value == 'races') {
-        let card = new Card();
-        card.createCard();
-        console.log('good')
+        card.createRacesCard();
+    }
+    if (value == '' && search.value !== 'races') {
+        card.destroyRacesCard();
     }
 }
 function addColorToSeacrhVariant(string, position, lengtH) {
@@ -44,15 +46,33 @@ function controlKeyDownAndPasteValuesToInput(EO) {
         EO.preventDefault();
     }
 }
+// добавить сообщение о отсуствии вариантов
+search.addEventListener('blur', function (EO) {
+   console.log(searchVariants.length) 
+});
 // при получении полного значения в input, формируем "карточку игры"
-
 class Card {
     constructor() {
-        this.wrapperCard = document.createElement('div');
+        this.racesCard = document.createElement('div');
+        this.racesCardImg = document.createElement('img');
+        this.racesCardTextContent = document.createElement('p');
         this.searchBlock = document.querySelector('.container__search__block');
     }
-    createCard() {
-        this.wrapperCard.classList.add("test");
-        this.searchBlock.appendChild(this.wrapperCard);
+    createRacesCard() {
+        this.racesCard.classList.add('races__active__block');
+        this.racesCard.classList.add('animation__games__search__blocks');
+        this.racesCardImg.setAttribute('src', './imgs/racesBlockImgMainPage.png');
+        this.racesCardImg.classList.remove('disable');
+        this.racesCardImg.classList.add('img__of__races__block');
+        this.racesCardTextContent.textContent = 'Races - игра в стиле 2D гонок. Вам предстоит как можно дольше продержаться на дороге, либо же, потерпеть неудачу, столкнувшись со встречной машиной. С правилами можно ознакомиться в самой игре. Удачи!';
+        this.racesCardTextContent.classList.add('text__content_of__races__card');
+        this.racesCard.appendChild(this.racesCardImg);
+        this.racesCard.appendChild(this.racesCardTextContent);
+        this.searchBlock.appendChild(this.racesCard);
+    }
+    destroyRacesCard() {
+        this.racesCard.classList.remove('races__active__block');
+        this.racesCardImg.classList.add('disable');
     }
 }
+let card = new Card();
