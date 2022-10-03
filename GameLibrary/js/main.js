899cef1d153cc07f682630af37985f0494095bd0
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-10-03 19:54:22 +0200

    main

--- GameLibrary/js/main.js
+++ GameLibrary/js/main.js
@@ -1,1 +1,58 @@
 "use strict";
+// делаем поиск игр
+let search = document.getElementById('search');
+search.oninput = function (EO) {
+    EO = EO || window.event;
+    search.value = search.value.toLowerCase(); // обрезаем пробелы и сводим всё к нижнему регистру, меняем спец.символы на html мнемоники
+    search.value = search.value.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
+    let value = this.value.trim();
+    let searchVariants = document.querySelectorAll('.search__variants li');
+    if (value != '') {
+        searchVariants.forEach(function (element) {
+            if (element.textContent.search(value) == -1) {
+                element.classList.add('disable');
+                element.innerHTML = element.textContent;
+            } 
+            else {
+                element.classList.remove('disable');
+                let string = element.textContent;
+                let nextSymbolsOfString = element.textContent.search(value);
+                element.innerHTML = addColorToSeacrhVariant(string, nextSymbolsOfString, value.length);
+            }
+        });
+    }
+    else {
+        searchVariants.forEach(function (element) {
+            element.classList.remove('disable');
+            element.innerHTML = element.textContent;
+        });
+    }
+    if (value == 'races') {
+        let card = new Card();
+        card.createCard();
+        console.log('good')
+    }
+}
+function addColorToSeacrhVariant(string, position, lengtH) {
+    return string.slice(0, position) + '<mark>' + string.slice(position, position + lengtH) + '</mark>' + string.slice(position + lengtH);
+}
+// на всякий случай полностью запрещаем ввод и вставку спец. символов
+search.addEventListener('paste', controlKeyDownAndPasteValuesToInput);
+search.addEventListener('keydown', controlKeyDownAndPasteValuesToInput);
+function controlKeyDownAndPasteValuesToInput(EO) {
+    if (['-', '{', '}', '*', '&', '[', ']', '#', '$', '%', '^', '(', ')', '=', '<', '>', '|', '?', '!', '`', '~', '@', '"', "'", '_', '+'].includes(EO.key)) {
+        EO.preventDefault();
+    }
+}
+// при получении полного значения в input, формируем "карточку игры"
+
+class Card {
+    constructor() {
+        this.wrapperCard = document.createElement('div');
+        this.searchBlock = document.querySelector('.container__search__block');
+    }
+    createCard() {
+        this.wrapperCard.classList.add("test");
+        this.searchBlock.appendChild(this.wrapperCard);
+    }
+}
