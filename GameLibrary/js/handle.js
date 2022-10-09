848a30886403f8c1f18be6da41152ee50170beee
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-10-09 14:27:13 +0200

    main

--- GameLibrary/js/handle.js
+++ GameLibrary/js/handle.js
@@ -230,6 +230,26 @@ arrayAllImgsOfDocument.forEach(element => {
     }
     });
 });
+// тоже самое для touch
+arrayAllImgsOfDocument.forEach(element => {
+    element.ondragstart = function () { return false } // отменяем перетаскивание картинок
+    element.onselectionchange = function () { return false } // отменяем выделение картинок
+    element.addEventListener('touchstart', function clickOnAnimal (EO) {
+    EO = EO || window.event;
+    mouseDownOnAnimal.play();
+    score++;
+    shadowScoreSpan.innerHTML = `Score: ${score}`;
+    scoreSpan.innerHTML = `Score: ${score}`;
+    flag = true;
+    if(flag) {
+        element.removeEventListener('click', clickOnAnimal);
+        setTimeout(function () {
+            flag = false;
+            element.addEventListener('click', clickOnAnimal);
+        }, 900);
+    }
+    });
+});
 // предупрждение о выходе и несохраннёных данных
 window.onbeforeunload = function() {
     return "При выходе со страницы, несохранённые данные могут быть потеряны. Выйти?";
