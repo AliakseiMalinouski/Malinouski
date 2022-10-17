0881b59dd8a9d5a6c5ae4f4b7da5060247d02814
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-10-17 18:06:25 +0200

    main

--- multiplication_table/script.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-let table = document.querySelector('.table');
-for (let i = 1; i < 10; i++) {
-    for (let j = 1; j < 10; j++) {
-        table.innerHTML += `${i}*${j} = ${j * i} <br>`;
-    }
-    table.innerHTML += `<hr>`;
-}
-
