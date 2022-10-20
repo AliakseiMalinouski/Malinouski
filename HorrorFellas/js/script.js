e44efa9b465a6d3153fac2fd0f1a686e057390c5
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-10-20 16:46:40 +0200

    main

--- HorrorFellas/js/script.js
+++ HorrorFellas/js/script.js
@@ -1,1 +1,21 @@
 "use strict"
+let nextButtonSlider = document.querySelector('.Next');
+let previousButtonSlider = document.querySelector('.Previous');
+let WrapperSlides = document.querySelector('.WrapperSlides');
+let distance = 0;
+nextButtonSlider.addEventListener('click', function (EO) {
+    EO = EO || window.event;
+    distance = distance + 1140;
+    if (distance >= 2280) {
+        distance = 0;
+    }
+    WrapperSlides.style.left = -distance + 'px';
+});
+previousButtonSlider.addEventListener('click', function (EO) {
+    EO = EO || window.event;
+    distance = distance - 1140;
+    if (distance < 0) {
+        distance = 1140;
+    }
+    WrapperSlides.style.left = -distance + 'px';
+});
