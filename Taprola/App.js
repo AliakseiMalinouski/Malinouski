97d23dfe9e1397ae852032bab8b9bb939b296a99
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-11-19 16:55:58 +0100

    React app development

--- Taprola/App.js
+++ Taprola/App.js
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Intrudaction from './components/IntrudactionComponent';
-// import arrayItem from './json/arrayItems.json';
-// import Taprola from './components/TaprolaComponent';
-// import colorsArray from './json/colorsArray.json';
 import {PagesRouter} from './router/PagesRouter';
 
 let newItemH = {};
 
 ReactDOM.render(
-    // <Taprola
-    //     array={arrayItem}
-    //     newItemH={newItemH}
-    //     arrayColors={colorsArray}
-    // />,
     <BrowserRouter>
         <Intrudaction />
         <PagesRouter/>
--- Taprola/components/IntrudactionComponent.js
+++ Taprola/components/IntrudactionComponent.js
@@ -7,18 +7,17 @@ import IconPallete from '../json/icon-pallete.json';
 
 class Intrudaction extends PureComponent {
     
-
     render() {
-            return <div className='WrapperWelcomeAlert'>
-                <h2 className='TitleWelcome'>Meet the Taprola!</h2>
-                <h4 className='UnderTitleMakeItEasy'><img src={CreateIcon} alt='Pencil' /> Make it easy</h4>
-                <p className='ParagraphAboutCreate'>To create a product,<br /> simply click on the "Add new item" button</p>
-                <h4 className='UnderTitleEdit'><img src={IconEdit} alt='Arrows' /> Change it</h4>
-                <p className='ParagraphAboutEdit'>To change item information,<br /> click on the "Edit item" button</p>
-                <h4 className='UnderTitlePalette'><img src={IconPallete} /> A varied palette!</h4>
-                <p className='ParagraphAboutPallete'>To change the colour of the header,click on the "colour palette" button</p>
-                <NavLink className='ButtonStartWork' to="/taprola" >Get to work</NavLink>
-            </div>
+        return <div className='WrapperWelcomeAlert'>
+            <h2 className='TitleWelcome'>Meet the Taprola!</h2>
+            <h4 className='UnderTitleMakeItEasy'><img src={CreateIcon} alt='Pencil' /> Make it easy</h4>
+            <p className='ParagraphAboutCreate'>To create a product,<br /> simply click on the "Add new item" button</p>
+            <h4 className='UnderTitleEdit'><img src={IconEdit} alt='Arrows' /> Change it</h4>
+            <p className='ParagraphAboutEdit'>To change item information,<br /> click on the "Edit item" button</p>
+            <h4 className='UnderTitlePalette'><img src={IconPallete} /> A varied palette!</h4>
+            <p className='ParagraphAboutPallete'>To change the colour of the header,click on the "colour palette" button</p>
+            <NavLink className='ButtonStartWork' to="/taprola">Get to work</NavLink>
+        </div>
     }
 }
 export default Intrudaction;
