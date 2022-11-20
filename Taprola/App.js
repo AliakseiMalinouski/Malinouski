3808299c169263867ebb10add65815f282c9b02c
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-11-20 16:21:54 +0100

    React App development

--- Taprola/App.js
+++ Taprola/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Intrudaction from './components/IntrudactionComponent';
-import {PagesRouter} from './router/PagesRouter';
+import { PagesRouter } from './router/PagesRouter';
+
 
 let newItemH = {};
 
 ReactDOM.render(
     <BrowserRouter>
-        <Intrudaction />
         <PagesRouter/>
     </BrowserRouter>,
     document.getElementById('container')
--- Taprola/router/PagesRouter.js
+++ Taprola/router/PagesRouter.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { PageTaprola } from '../pages/PageTaprola';
+import { PageIntrudaction } from '../pages/PageIntrudaction';
 
 export const PagesRouter = () => {
           
     return (
       <Routes>
+        <Route path='/' element={<PageIntrudaction/>} />
         <Route path="/taprola" element={<PageTaprola/>} />
       </Routes>
     );
