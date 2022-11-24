ab8f0ede03c2fef3983f2d0fa5d12079cc40be18
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-11-24 20:12:56 +0100

    React App development

--- Taprola/App.js
+++ Taprola/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { PagesRouter } from './router/PagesRouter';
-
+import './src/i18n/config';
 
 let newItemH = {};
 
--- Taprola/components/OptionsComponent.js
+++ Taprola/components/OptionsComponent.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import MobileIcon from '../json/icon-modileopt.json';
 import LanguageIcon from '../json/icon-language.json';
+import { withTranslation } from 'react-i18next';
 
 class Options extends React.PureComponent {
     static propTypes = {
@@ -16,6 +17,14 @@ class Options extends React.PureComponent {
         this.setState({isLanguage: true})
     }
 
+    setEN = () => {
+        this.props.i18n.changeLanguage("en");
+    }
+
+    setRU = () => {
+        this.props.i18n.changeLanguage("ru");
+    }
+
     render() {
         return <div className='WrapperOptions'>
             <div className='Illustration'>
@@ -23,13 +32,13 @@ class Options extends React.PureComponent {
                 <img src={MobileIcon} alt="Smartphone" />
             </div>
             <div className='WrapperCatigories'>
-                <div onClick={this.setLanguage} style={{ display: 'flex', alignItems: 'center' }}><img style={{ width: '40px', height: '40px', marginRight: '15px' }} src={LanguageIcon} />Language</div>
+                <div onClick={this.setLanguage} style={{ display: 'flex', alignItems: 'center' }}><img style={{ width: '40px', height: '40px', marginRight: '15px' }} src={LanguageIcon} />{this.props.t("language")}</div>
                 {
                     (this.state.isLanguage) 
                         ?
                     <div className='LanguageButtons'>
-                        <button>EN</button>
-                        <button>RU</button>
+                        <button type='button' onClick={this.setEN}>EN</button>
+                        <button type='button' onClick={this.setRU}>RU</button>
                     </div>
                         :
                     null
@@ -38,4 +47,4 @@ class Options extends React.PureComponent {
         </div>
     }
 }
-export default Options;
+export default withTranslation() (Options);
