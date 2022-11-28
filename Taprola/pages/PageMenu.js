ca3512f1cfcd13a48f7cf1ecb77907e3853dce16
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-11-28 20:28:52 +0100

    React app development

--- Taprola/pages/PageMenu.js
+++ Taprola/pages/PageMenu.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MenuComponent from '../components/MenuComponent';
-import ArrayCategories from '../json/arrayCategories.json';
 
-export const PageMenu =  () => {
+export const PageMenu = () => {
+    
+    const [array, setArray] = useState([]);
+
+    useEffect(() =>
+        {
+        fetch("https://gist.githubusercontent.com/AliakseiMalinouski/57225c6273781cf6e6f858cbf5cb59fd/raw/10f1b5fa10883c4abb0f246a814716996f1ff074/ArrayCategoryTaprola",
+            { method: 'get' })
+            .then(response => {
+                if (!response.ok) {
+                alert("Error with require date");
+                }
+                else {
+                    return response.json()
+                }
+            })
+            .then(data => {
+                setArray(data);
+        })
+    }, 
+        []
+    )
     return (
-        <MenuComponent array={ArrayCategories} />
+        <MenuComponent array={array} />
     )
 }
