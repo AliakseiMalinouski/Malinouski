44376c492d5777651fa9f467e5adedf2e9f6fc9f
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-11-29 14:21:25 +0100

    React app development

--- Taprola/components/MenuComponent.js
+++ Taprola/components/MenuComponent.js
@@ -5,19 +5,30 @@ import { NavLink } from 'react-router-dom';
 
 class Menu extends React.PureComponent {
 
-
     static propTypes = {
         array: PropTypes.array.isRequired
     }
 
+    
+    state = {
+        searchValue: "",
+    }
+
+    setSearchValue = (EO) => {
+        let value = EO.target.value;
+        this.setState({ searchValue: value });
+    }
+
     render() {
-        let categories = this.props.array.map(e => 
-            <Categories key={e.code} name={e.name} images={e.photos} code={e.code} className={e.className} />
-            )
         return <div className='WrapperMenu'>
             <h2 className='Title'>Your categories</h2>
+            <input type='text' value={this.state.searchValue} onChange={this.setSearchValue} />
             <div className='WrapperCategories' >
-                {categories}
+                {
+                    this.props.array.filter(element => {
+                        return element.name.toLowerCase().includes(this.state.searchValue.toLowerCase());
+                    }).map(e => <Categories key={e.code} name={e.name} images={e.photos} code={e.code} className={e.className} />)
+                }
             </div>
             <NavLink to="/taprola">Back</NavLink>
         </div>
