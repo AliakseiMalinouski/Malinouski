657a5ff70c6799561b3281f47ca78aa9ae81f235
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-12-01 19:54:45 +0100

    React app development

--- Taprola/components/MenuComponent.js
+++ Taprola/components/MenuComponent.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Categories from './CategoriesComponent';
 import { NavLink } from 'react-router-dom';
