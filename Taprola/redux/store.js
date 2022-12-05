bf2a0994ac768b151d66246e6601e8a15de73d32
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-12-05 15:49:11 +0100

    React app development

--- Taprola/redux/store.js
+++ Taprola/redux/store.js
@@ -1,11 +1,10 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import infoCategorySlice from './infoCategorySlice';
-import userSlice from './userSlice';
+
 
 export const store = configureStore({
     reducer: {
         informationAboutCategory: infoCategorySlice,
-        user: userSlice
     }
 })
