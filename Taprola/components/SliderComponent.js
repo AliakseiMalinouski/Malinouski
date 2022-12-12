2b2a3b27b8f3ab1e904ce4d301e9028430d1af95
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-12-12 19:14:13 +0100

    React app development

--- Taprola/components/SliderComponent.js
+++ Taprola/components/SliderComponent.js
@@ -11,19 +11,35 @@ export const Slider = () => {
     let dispatch = useDispatch();
 
     const [leftSliderTrack, setWidthSliderTrack] = useState(0);
+    const [isClickedPreviousButton, setIsClickedPreviousButton] = useState(false);
+    const [isClickedNextButton, setIsClickedNextButton] = useState(false);
 
     const staticLeft = 760;
 
     const previous = () => {
-        console.log('prev')
         setWidthSliderTrack(prev => prev + staticLeft);
         leftSliderTrack >= 0 ? setWidthSliderTrack(-1520) : null;
+        setIsClickedPreviousButton(true);
     }
 
     const next = () => {
         setWidthSliderTrack(prev => prev - staticLeft);
         leftSliderTrack <= -1140 ? setWidthSliderTrack(0) : null;
+        setIsClickedNextButton(true);
     }
+
+    useEffect(() => {
+        if (isClickedNextButton) {
+            setTimeout(() => {
+                setIsClickedNextButton(false);
+            }, 300);
+        }
+        else if (isClickedPreviousButton) {
+            setTimeout(() => {
+                setIsClickedPreviousButton(false);
+            }, 300);
+        }
+    }, [isClickedNextButton, isClickedPreviousButton]);
 
     useEffect(() => {
         fetch("https://gist.githubusercontent.com/AliakseiMalinouski/d6798bcc83126b681ebe4721d0adfc68/raw/ebff38fec9d70900f5b4674733b27434dc20f54e/GalleryTaprolaArray", { method: 'get' })
@@ -44,17 +60,18 @@ export const Slider = () => {
         <div className='WrapperSlider'>
             <div className='Slider'>
                 <div className='SliderTrack' style={{left: leftSliderTrack + 'px'}}>
-                    {/* <div className='Slide1'></div>
-                    <div className='Slide2'></div>
-                    <div className='Slide3'></div> */}
                     {
                         galleryArray.data.map(e => <GallerySlides key={e.code} code={e.code} name={e.name} description={e.description} image={e.image} />)
                     }
                 </div>
             </div>
             <div className='ButtonsGallery'>
-                <button type='button' onClick={previous}>{`${'<'}`}</button>
-                <button type='button' onClick={next}>{`${'>'}`}</button>
+                {
+                    !isClickedPreviousButton ? <button type='button' onClick={previous}>{`${'<'}`}</button> : <button type='button' onClick={previous} style={{backgroundColor: 'red', opacity: '1'}}>{`${'<'}`}</button>
+                }
+                {
+                    !isClickedNextButton ? <button type='button' onClick={next}>{`${'>'}`}</button> : <button type='button' onClick={next} style={{backgroundColor: 'red', opacity: '1'}}>{`${'>'}`}</button>
+                }
             </div>
         </div>
     )
