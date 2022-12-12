453721b8c3877c4e19880bd2d6be25a0ce3f655c
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-12-12 19:03:28 +0100

    React app development

--- Taprola/components/GallerySlidesComponent.js
+++ Taprola/components/GallerySlidesComponent.js
@@ -3,9 +3,9 @@ import React from "react";
 export const GallerySlides = ({ code, name, description, image }) => {
     return (
         <div>
-            <div className='Slide' style={{backgroundImage: `url(${image})`, backgroundSize: '100% 100%', backgroundRepeat: 'no-repeat'}}>
-                <h3>{name}</h3>
-                <p>{description}</p>
+            <div className='Slide' style={{backgroundImage: `url(${image})`, backgroundSize: '150% 150%', backgroundRepeat: 'no-repeat'}}>
+                <h3 className='Name'>{name}</h3>
+                <p className='Description'>{description}</p>
             </div>
         </div>
     )
