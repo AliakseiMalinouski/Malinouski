1aabc05a3a68cafaa6c9ee9bc92bf973e42cb7c9
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-12-14 16:52:37 +0100

    React app development

--- taprolaapp/src/components/AuthComponent.js
+++ taprolaapp/src/components/AuthComponent.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { auth } from '../firebase-config';
 
 export const Auth = () => {
+
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+    const [user, setUser] = useState({});
+
+    onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+    });
+
+    const register = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+            console.log(user)
+        }
+        catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    const login = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+            console.log(user)
+        }
+        catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    const logout = async () => {
+        await signOut(auth);
+    }
+
     return (
         <div className='WrapperAuth'>
             <div>
                 <h3>Register User</h3>
-                <input placeholder='email'/>
-                <input placeholder='password' />
-                <button>Create User</button>
+                <input placeholder='email' onChange={(EO) => { setRegisterEmail(EO.target.value) }}  />
+                <input placeholder='password' onChange={(EO) => { setRegisterPassword(EO.target.value) }} />
+                <button onClick={register}>Create User</button>
             </div>
             <div>
                 <h3>Login</h3>
-                <input placeholder='email'/>
-                <input placeholder='password' />
-                <button>Login</button>
+                <input placeholder='email' onChange={(EO) => { setLoginEmail(EO.target.value) }} value={loginEmail} />
+                <input placeholder='password' onChange={(EO) => { setLoginPassword(EO.target.value) }} value={loginPassword}/>
+                <button onClick={login}>Login</button>
             </div>
-            <h4>User Logged in:</h4>
-            <button>Sign out</button>
+            <h4>User Logged in: {user?.email}</h4>
+            <button onClick={logout}>Sign out</button>
         </div>
     )
 }
