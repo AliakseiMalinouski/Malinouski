8fb209522cfcfedcd3e01d739ab013d4b02c73cb
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-12-14 16:15:33 +0100

    React app development

--- taprolaapp/src/components/AuthComponent.js
+++ taprolaapp/src/components/AuthComponent.js
@@ -4,8 +4,21 @@ import { useSelector, useDispatch } from 'react-redux';
 
 export const Auth = () => {
     return (
-        <div>
-            some text
+        <div className='WrapperAuth'>
+            <div>
+                <h3>Register User</h3>
+                <input placeholder='email'/>
+                <input placeholder='password' />
+                <button>Create User</button>
+            </div>
+            <div>
+                <h3>Login</h3>
+                <input placeholder='email'/>
+                <input placeholder='password' />
+                <button>Login</button>
+            </div>
+            <h4>User Logged in:</h4>
+            <button>Sign out</button>
         </div>
     )
 }
