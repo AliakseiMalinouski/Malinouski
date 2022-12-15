ee7a2b7c8fa8f9383d4568bcba98c557f73243f2
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-12-15 16:39:41 +0100

    React app development

--- taprolaapp/src/components/TaprolaComponent.js
+++ taprolaapp/src/components/TaprolaComponent.js
@@ -188,7 +188,8 @@ class Taprola extends React.PureComponent {
                 this.setState({ disabledAddNewItemButton: true });
             }
         }
-        this.state.ItemH.iconWasteUrl = "https://cdn-icons-png.flaticon.com/512/6559/6559406.png";
+        // this.state.ItemH.iconWasteUrl = "https://cdn-icons-png.flaticon.com/512/6559/6559406.png";
+        this.setState({ ItemH: { ...this.state.ItemH, iconWasteUrl: "https://cdn-icons-png.flaticon.com/512/6559/6559406.png" } });
     }
 
     addNewItem = (EO) => {
@@ -385,8 +386,8 @@ class Taprola extends React.PureComponent {
                             :
                             <button type='button' className='EditItemButton' disabled>{this.props.t("edit-item")}</button>
                     }
-                    <NavLink to="/"><img src={IconBack} className='ButtonrReturnToIntrudaction'/></NavLink>
-                    <NavLink to="/options"><img src={OptionsIcon} className='ButtonGoToOptions' /></NavLink>
+                    <NavLink to="/"><img src={IconBack} className='ButtonrReturnToIntrudaction' alt='Image'/></NavLink>
+                    <NavLink to="/options"><img src={OptionsIcon} className='ButtonGoToOptions' alt='Image'/></NavLink>
                     <img onClick={this.setLanguage} className="LanguageButton" src={LanguageIcon} alt='Language' />
                     <NavLink to="/menu"><img src={MenuIcon} alt='Menu' className='ButtonGoToMenu' /></NavLink>
                     <NavLink to='/send'><img className='SendIcon' src={SendIcon} alt='Send' /></NavLink>
