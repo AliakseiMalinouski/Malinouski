ac8cfe62a57123fb14da2cca50c9d6b713eb61be
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-12-15 17:19:25 +0100

    React app development

--- taprolaapp/src/components/IntrudactionComponent.js
+++ taprolaapp/src/components/IntrudactionComponent.js
@@ -15,7 +15,7 @@ export const Intrudaction = React.memo(() => {
     const list = useSelector(state => state.informationAboutListIntrudaction);
 
     useEffect(() => {
-        fetch("https://gist.githubusercontent.com/AliakseiMalinouski/5373be1277141069e4b420f55c1c68ea/raw/85daa398d6f7dc955b75261e2e867c66cbfa2832/IntrudactionListTaprola", { method: 'get' })
+        fetch("https://gist.githubusercontent.com/AliakseiMalinouski/5373be1277141069e4b420f55c1c68ea/raw/6eae4b2bee9026a34644a72597f251411a1f5877/IntrudactionListTaprola", { method: 'get' })
             .then(response => {
                 if (!response.ok) {
                     alert("Error")
@@ -32,10 +32,10 @@ export const Intrudaction = React.memo(() => {
     return (
         <div className='WrapperWelcomeAlert'>
             <h2 className='TitleWelcome'>Meet the Taprola!</h2>
-        {
-            list.data.map(e => <IntrudactionList key={e.code} image={e.image} title={e.title} paragraph={e.paragraph} classNameParagraph={e.classNameParagraph} classNameTitle={e.classNameTitle} />)
-        }
-        <NavLink className='ButtonStartWork' to="/taprola">Get to work</NavLink>
+            {
+                list.data.map(e => <IntrudactionList key={e.code} image={e.image} title={e.title} paragraph={e.paragraph} classNameParagraph={e.classNameParagraph} classNameTitle={e.classNameTitle} />)
+            }
+            <NavLink className='ButtonStartWork' to="/taprola">Get to work</NavLink>
         </div>
     )
 })
