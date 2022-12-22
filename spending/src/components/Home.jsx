c0e6f7edf75a78f3ad31018e8ee6d34dafdb18f9
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-12-22 19:31:50 +0100

    React app development

--- spending/src/components/Home.jsx
+++ spending/src/components/Home.jsx
@@ -10,6 +10,7 @@ export const Home = () => {
     const [cash, setCash] = useState(80000000);
     const [currentPrice, setCurrentPrice] = useState(null);
     const [active, setActive] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
 
     const cashChangd = useRef();
 
@@ -25,7 +26,7 @@ export const Home = () => {
     let dispatch = useDispatch();
 
     useEffect(() => {
-        let url = "https://gist.githubusercontent.com/AliakseiMalinouski/16cd68b98a0e568dadbcf4b5c29ab385/raw/0bf4184a9820bf2e43379b5f2fe4eca670c4e291/BreakingBadBaseArray"
+        let url = "https://gist.githubusercontent.com/AliakseiMalinouski/16cd68b98a0e568dadbcf4b5c29ab385/raw/e2c73737d9da1cd49ba840802c121f3fda226c63/BreakingBadBaseArray"
         fetch(url, { method: 'get' })
             .then(response => {
                 if (!response.ok) {
@@ -75,9 +76,17 @@ export const Home = () => {
             </CSSTransition></div>
         </div>
         <div className="HomePage">
+            <div className="Search">
+                <h4>Enter a name</h4>
+                <input type='text' value={searchValue} placeholder='name' onChange={(eo) => {
+                    setSearchValue(eo.target.value);
+                }} />
+            </div>
         <div className="WrapperItems">
             {
-                itemsList.map(e => <Items key={e.code} cbIn={buyItem} cbDc={sellItem} code={e.code} name={e.name} image={e.image} price={e.price} quanlity={e.quanlity} buy={e.buy} sell={e.sell} />)
+                itemsList.filter(element => {
+                    return element.name.toLowerCase().includes(searchValue.toLowerCase())
+                }).map(e => <Items key={e.code} cbIn={buyItem} cbDc={sellItem} code={e.code} name={e.name} image={e.image} price={e.price} quanlity={e.quanlity} buy={e.buy} sell={e.sell} />)
             }
         </div>
     </div>
