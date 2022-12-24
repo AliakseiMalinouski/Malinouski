2fc9744846ebd31a879c65c2bc66eb575edfbae4
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-12-24 20:11:58 +0100

    React app development

--- spending/src/Pages/PageHome.js
+++ spending/src/Pages/PageHome.js
@@ -7,7 +7,6 @@ export const PageHome = () => {
             <div className="ImageMainCharacter"></div>
             <div className="Phrases">
             <p>The good guys are never written about as much as the bad guys.</p>
-            <p>Here you have the opportunity to spend money on Breaking Bad's main character Walter White by buying items from the series</p>
         </div>
             <Home/>
         </div>
