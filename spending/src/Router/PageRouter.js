c2cbdf194c2611ff7b7fa870ca08b17a95ddf285
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2022-12-28 15:03:35 +0100

    React app development

--- spending/src/Router/PageRouter.js
+++ spending/src/Router/PageRouter.js
@@ -2,12 +2,10 @@ import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { PageHome } from "../Pages/PageHome";
 import { PageAuth } from "../Pages/PageAuth";
-import { PageIntrudaction } from "../Pages/PageIntrudaction";
 
 export const PageRouter = () => {
     return (
         <Routes>
-            <Route path="/intrudaction-page" element={<PageIntrudaction/>} />
             <Route path="/registration-page" element={<PageAuth/>} />
             <Route path="/" element={<PageHome/>} />
         </Routes>
