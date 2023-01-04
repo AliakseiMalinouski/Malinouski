073d11194b9769184f8593d57e95aa7a0c4ee184
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-01-04 17:32:51 +0100

    React app development

--- randomizerfilms/src/Redux/loadFilms.js
+++ randomizerfilms/src/Redux/loadFilms.js
@@ -3,7 +3,7 @@ import { getFilms, updateLoadState } from "./filmsSlice";
 
 export const loadFilms = (dispatch) => {
     dispatch(updateLoadState({ loadState: 1 }));
-    fetch("https://gist.githubusercontent.com/AliakseiMalinouski/fa0d16adc01043d44e5e374f9bb7bfd8/raw/cb0419260faae0f1315517b122d6de8bcd1a2ba1/FilmsArray",
+    fetch("https://gist.githubusercontent.com/AliakseiMalinouski/fa0d16adc01043d44e5e374f9bb7bfd8/raw/8b7074e65ceeaf8a47a8aed74dc8c1ba1b2166f8/FilmsArray",
         {
             method: 'get',
             headers: {
