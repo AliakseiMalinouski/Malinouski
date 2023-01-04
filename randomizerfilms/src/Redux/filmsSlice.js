6af18d2bb55160a7b1ea22caac157d84f6f3eb55
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-01-04 16:39:46 +0100

    React app development

--- randomizerfilms/src/Redux/filmsSlice.js
+++ randomizerfilms/src/Redux/filmsSlice.js
@@ -15,11 +15,8 @@ export const filmsSlice = createSlice({
         updateLoadState: (state, action) => {
             state.loadState = action.payload.loadState;
         },
-        changeFilm: (state, action) => {
-
-        }
     }
 })
 
-export const { getFilms, updateLoadState, changeFilm } = filmsSlice.actions;
+export const { getFilms, updateLoadState } = filmsSlice.actions;
 export default filmsSlice.reducer;
--- randomizerfilms/src/components/Film.jsx
+++ randomizerfilms/src/components/Film.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { filmsEvents } from '../events';
+import { useEffect } from 'react';
 
-export const Film = ({ code, name, year, duration, genre, director, actors, image, plot, rating , workMode}) => {
+export const Film = ({ code, name, year, duration, genre, director, actors, image, plot, rating}) => {
     
+    useEffect(() => {
+        if (code === undefined || name === undefined || year === undefined || duration === undefined || genre === undefined || director === undefined || actors === undefined || image === undefined || plot === undefined || rating === undefined) {
+            filmsEvents.emit('ViewNewFilm');
+        }
+    }, [code, name, year, duration, genre, director, actors, image, plot, rating]);
+
+
     const viewNewFilm = () => {
         filmsEvents.emit('ViewNewFilm');
     }
