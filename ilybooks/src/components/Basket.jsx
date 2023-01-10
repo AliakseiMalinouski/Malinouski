684b2c20f0687b3b845cdcbbe7bc881175f7d1cb
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-01-10 18:34:29 +0100

    React app development

--- ilybooks/src/components/Basket.jsx
+++ ilybooks/src/components/Basket.jsx
@@ -2,22 +2,31 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { BookBasket } from './BookBasket';
 import { clearBasket } from '../Redux/basketSlice';
+import { useState, useEffect } from 'react';
 
 export const Basket = React.memo(() => {
 
-    let dispatch = useDispatch();
-
-
     const items = useSelector(state => state.basket.basket);
+    const [total, setTotal] = useState(null);
+
+    useEffect(() => {
+        setTotal((9.99 * items.length) + 4.99);
+    }, [items, total, setTotal])
+
+    let dispatch = useDispatch();
 
     const resetBasket = () => {
         let proofDelete = window.confirm("Are you sure?");
         if(proofDelete) dispatch(clearBasket());
     }
 
+    
+
 
     return (
-        <div className='BasketBooks'>
+        <div className='Cart'>
+            <div className='BasketBooks'>
+            <h5>Home / Cart</h5>
             {
                 (!items.length)
                     ?
@@ -33,6 +42,28 @@ export const Basket = React.memo(() => {
                     </div>
                     :
                     items.map(e => <BookBasket key={e.code} code={e.code} name={e.name} image={e.image} arrow={e.arrow} type={e.type}/>)
+            }
+            </div>
+            {
+                (items.length)
+                    ?
+                <div className='InformationAboutBookBasket'>
+                    <div className='Summary'>
+                    <h4>Summary</h4>
+                    <h5>
+                        Subtotal: <span>9.99$</span>
+                    </h5>
+                    <h5>
+                        Delivery: <span>4.99$</span>
+                    </h5>
+                    </div>
+                    <div className='Total'>
+                        <h5>Total: <span>{total}$</span></h5>
+                        <button>Checkout</button>
+                    </div>
+                </div>
+                    :
+                    null
             }
         </div>
     )
