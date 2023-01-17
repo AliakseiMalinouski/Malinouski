f7a5141d1c3ef85ef92b07dbd40c1c8b3a5cd317
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-01-17 17:25:37 +0100

    React app development

--- memes-generator/src/components/CurrentMeme.jsx
+++ memes-generator/src/components/CurrentMeme.jsx
@@ -9,14 +9,13 @@ export const CurrentMeme = ({name, code, url}) => {
         setTopText(EO.target.value);
     }
 
-
     return (
         <div className='CurrentMeme'>
-            <div className='ImageMeme' style={{backgroundImage: `url(${url})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center'}}>
-                <input type='text' maxLength='30' className='TopText' placeholder='Top Text' value={topText} onChange={validationTopText}/>
+            <div id='capture' className='ImageMeme' style={{backgroundImage: `url(${url})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center'}}>
+                <span className='TopText'>{topText}</span>
             </div>
             <div className='MemeDevTools'>
-
+                <input type='text' className='TopTextInput' maxLength='30' placeholder='Top Text' value={topText} onChange={validationTopText}/>
             </div>
         </div>
     )
