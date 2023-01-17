a50d4688ce8803f99016dacd6e9d235752b81630
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-01-17 20:20:18 +0100

    React app development

--- weather-application/src/components/Data.jsx
+++ weather-application/src/components/Data.jsx
@@ -1,40 +1,69 @@
 import React from "react";
+import { useEffect } from "react";
 
 export const Data = React.memo(({name, wind, weather, main}) => {
 
-    let currentTemperature = Math.round(main.temp);
+    useEffect(() => {
+        createDate();
+    }, []);
 
-    let currentTemperatureMax = Math.round(main.temp_max);
+    const weatherData = {
+        currentTemperature: Math.round(main.temp),
+        currentTemperatureMax: Math.round(main.temp_max),
+        currentTemperatureMin: Math.round(main.temp_min),
+        feelsLikeTemperature: Math.round(main.feels_like),
+        currentGust: (wind.gust === undefined ? null : Math.round(wind.gust)),
+        currentPressure: Math.round(main.pressure),
+        windSpeed: wind.speed,
+    }
 
-    let currentTemperatureMin = Math.round(main.temp_min);
+    const createDate = () => {
+        let dateHash = new Date();
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        
+        let day = days[dateHash.getDay()];
+        let date = dateHash.getDate();
+        let mounth = months[dateHash.getMonth()];
+        let year = dateHash.getFullYear();
 
-    let feelsLikeTemperature = Math.round(main.feels_like);
+        return `${day} ${date} ${mounth} ${year}`
+    }
 
-    let currentGunt = Math.round(wind.gust);
-
-    let currentPressure = Math.round(main.pressure);
+    console.log(createDate())
 
     return (
         <div className="Data">
-            <h3>{name}</h3>
-            <h4 className="temperature">
-               {currentTemperature}
-            </h4>
-            <p>{weather.main}</p>
-            <p>
-                <span>Min: {(currentTemperatureMin === currentTemperatureMax ? currentTemperatureMin - 1 : currentTemperatureMin)}</span>
-                <span>Max: {currentTemperatureMax}</span>
-            </p>
-            <p>
-                Feels like: {feelsLikeTemperature}
-            </p>
-            <p>
-                Wind speed: {wind.speed}
-            </p>
-            <p>
-                A gust of wind: {currentGunt} {currentGunt === 1 ? 'point' : "points"}
-            </p>
-            <h5>Pressure: {currentPressure}</h5>
+            <div className="CurrrentDate">
+
+            </div>
+            <div className="AboutWeather">
+                <h3>{name}</h3>
+                <h4 className="temperature">
+                {weatherData.currentTemperature}
+                </h4>
+                <p>{weather.main}</p>
+                <p>
+                    <span>Min: {(weatherData.currentTemperatureMin === weatherData.currentTemperatureMax ? weatherData.currentTemperatureMin - 1 : weatherData.currentTemperatureMin)}</span>
+                    <span>Max: {weatherData.currentTemperatureMax}</span>
+                </p>
+                <p>
+                    Feels like: {weatherData.feelsLikeTemperature}
+                </p>
+                <p>
+                    Wind speed: {weatherData.windSpeed} m/s
+                </p>
+                {
+                    (weatherData.currentGust === null)
+                    ?
+                    null
+                    :
+                    <p>
+                        A gust of wind: {weatherData.currentGust} {weatherData.currentGust === 1 ? 'point' : "points"}
+                    </p>
+                }
+                <h5>Pressure: {weatherData.currentPressure}</h5>
+            </div>
         </div>
     )
 })
