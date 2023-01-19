4a01abd9a9f48f9ac4461549c9a637441fb5a1fe
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-01-19 17:36:29 +0100

    React app development

--- weather-application/src/Redux/backgroundThunk.js
+++ weather-application/src/Redux/backgroundThunk.js
@@ -1,7 +1,7 @@
 import { getBackgrounds } from "./backgroundsSlice";
 
 export const backgroundThunk = (dispatch) => {
-    fetch("https://gist.githubusercontent.com/AliakseiMalinouski/883db6aebf765fa6618840b850bfb6f0/raw/4e6ddec5216da5c016f5f126d3f2d3b1fc7db95d/BackgroundWeatherApp", {method: 'get'})
+    fetch("https://gist.githubusercontent.com/AliakseiMalinouski/883db6aebf765fa6618840b850bfb6f0/raw/be79cdaa8d21730ef8cd898947a703f78da8ee4f/BackgroundWeatherApp", {method: 'get'})
     .then(response => {
         if(!response.ok) {
             alert("Error with download");
